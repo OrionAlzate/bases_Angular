5868b284fa42ca4852fc999a1da7f95b13453023
OrionAlzate <orion_alzate@hotmail.com>
2024-09-26 23:03:25 -0500

    Interpolación y métodos

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  public name : string = "super man";
+  public nameCap : string = this.name.toUpperCase();
+  public age : number = 45;
+
+
+  get captName():string{
+    return this.name.toUpperCase();
+
+  }
+
+  printName(word : string):void{
+    console.log(word)
+  }
+
 
 }
