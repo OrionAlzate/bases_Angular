67f48e3fc13b0a6d2d11e28c516b3ad328f681a7
OrionAlzate <orion_alzate@hotmail.com>
2024-09-27 21:34:06 -0500

    list component ngFor() ok

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-heros-hero',
@@ -6,9 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-  public name : string = "super man";
+  public initialName = "super man"
+  public name : string = this.initialName;
   public nameCap : string = this.name.toUpperCase();
-  public age : number = 45;
+  initialAge: number = 45;
+  public age : number = this.initialAge;
+  public updatedAge : number = 30;
+  public updatedName : string = "Sayaman";
 
 
   get captName():string{
@@ -19,6 +24,38 @@ export class HeroComponent {
   printName(word : string):void{
     console.log(word)
   }
+
+
+  get heroDescription():string{
+    return `${this.name} - ${this.age}`
+  }
+
+  getMethodName():string{
+    return `${this.age * 2}`
+  }
+
+  changeHero():void{
+    this.name = this.updatedName
+  }
+
+  changeAge():void{
+    this.age = this.updatedAge
+  }
+
+  changeForm() {
+    this.name = this.initialName
+    this.age = this.initialAge
+
+    // const selector = document.getElementById('title_hero');
+    // selector!.innerHTML = "Reseteo el nombre de la app"
+
+
+    // document.querySelectorAll('h1').forEach( element => {
+    //   element.innerHTML= "Desde Angular";
+    // })
+
+  }
+
 
 
 }
--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public heroesList : string[] = ['Spiderman', 'Ironman', 'Scartlett Witch', 'Hulk', 'Thor', 'Mario Bros']
+
+
+  addHero() : void {
+
+    let heroAdded : string | null = prompt('Ingrese el nombre de un Heroe', 'Deadpool') ;
+    // let elementHtml  : string = document.getElementById('hero_input')!.innerHTML;
+    if (heroAdded != null){
+      // elementHtml = ``
+      this.heroesList.push(heroAdded);
+    }
+
+  }
+
+  deleteHero() : void {
+      this.heroesList.pop();
+  }
 }
