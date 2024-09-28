645cbc8ce1aa5ee347fd1abf84e8e72f734a4b22
OrionAlzate <orion_alzate@hotmail.com>
2024-09-27 21:51:24 -0500

    list component get heroDeleted() is done

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -9,7 +9,7 @@ export class ListComponent {
 
   public heroesList : string[] = ['Spiderman', 'Ironman', 'Scartlett Witch', 'Hulk', 'Thor', 'Mario Bros']
 
-
+  public deletedHero : string = "";
   addHero() : void {
 
     let heroAdded : string | null = prompt('Ingrese el nombre de un Heroe', 'Deadpool') ;
@@ -22,6 +22,14 @@ export class ListComponent {
   }
 
   deleteHero() : void {
-      this.heroesList.pop();
+
+    if(this.heroesList.length > 0 ){
+      this.deletedHero = this.heroesList.pop()!;
+    }
   }
+
+  get heroDeleted(){
+    return this.deletedHero;
+  }
+
 }
